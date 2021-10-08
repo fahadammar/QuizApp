fd127bd63398ebe028d5930bc4a7b47cbf8e4f7c
Fahad Ammar <fahadammar96@outlook.com>
2021-10-09 02:31:06 +0500

    created the UI and connected the model-Data with the UI

--- app/src/main/java/com/example/quizapp/Activities/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/Activities/QuizQuestionsActivity.kt
@@ -3,14 +3,36 @@ package com.example.quizapp.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.quizapp.Class.Question
 import com.example.quizapp.Constants.Constants
 import com.example.quizapp.R
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        // questionList will get the array containing the data class Question constructors with values initialized
         val questionList = Constants.getQuestions()
         Log.i("QuestionList", "Size => ${questionList.size}" )
+
+
+        val currentPosition = 1
+        val question: Question? = questionList[currentPosition - 1]
+
+        // progressBar id -> accessing the property progess & progressBar.max
+        progressBar.progress = currentPosition
+        // Number which is right to progressBar
+        progressNum.text = "$currentPosition" + "/" + progressBar.max
+        // top question text ID
+        questionText.text = question!!.question
+        // flag image
+        flagImage.setImageResource(question.image) // the image is Int basically - for further info do search about it
+        // choice ID's
+        optionOne.text = question.optionOne
+        optionTwo.text = question.optionTwo
+        optionThree.text = question.optionThree
+        optionFour.text = question.optionFour
     }
 }
--- app/src/main/java/com/example/quizapp/Constants/Constant.kt
+++ app/src/main/java/com/example/quizapp/Constants/Constant.kt
@@ -7,94 +7,7 @@ import java.util.*
 object Constants {
 
     fun getQuestions(): ArrayList<Question> {
-        val questionList = ArrayList<Question>()
-        // 1
-        questionList.add(Question(
-            1, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_argentina,
-            "Argentina", "Australia",
-            "Armenia", "Austria", 1
-        )
-        )
-        // 2
-        questionList.add(Question(
-            2, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_australia,
-            "Angola", "Austria",
-            "Australia", "Armenia", 3
-        )
-        )
-        // 3
-        questionList.add( Question(
-            3, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_brazil,
-            "Belarus", "Belize",
-            "Brunei", "Brazil", 4
-        )
-        )
-        // 4
-        questionList.add( Question(
-            4, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_belgium,
-            "Bahamas", "Belgium",
-            "Barbados", "Belize", 2
-        )
-        )
-        // 5
-        questionList.add( Question(
-            5, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_fiji,
-            "Gabon", "France",
-            "Fiji", "Finland", 3
-        )
-        )
-        // 6
-        questionList.add( Question(
-            6, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_germany,
-            "Germany", "Georgia",
-            "Greece", "none of these", 1
-        )
-        )
-        // 7
-        questionList.add( Question(
-            7, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_denmark,
-            "Dominica", "Egypt",
-            "Denmark", "Ethiopia", 3
-        )
-        )
-        // 8
-        questionList.add( Question(
-            9, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_new_zealand,
-            "Australia", "New Zealand",
-            "Tuvalu", "United States of America", 2
-        )
-        )
-
-        // 9
-        questionList.add( Question(
-            10, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_kuwait,
-            "Kuwait", "Jordan",
-            "Sudan", "Palestine", 1
-        )
-        )
-        // 10
-        questionList.add(Question(
-            8, "What country does this flag belong to?",
-            R.drawable.ic_pakistan,
-            "Ireland", "Iran",
-            "Hungary", "Pakistan", 4
-        )
-        )
-
-
-        return questionList
-    }
-
-    private fun listOfQuestions():ArrayList<Question>{
+        // questionList will contain the constructors of class Question
         val questionList = ArrayList<Question>()
         // 1
         questionList.add(Question(
@@ -181,4 +94,5 @@ object Constants {
 
         return questionList
     }
+
 }
